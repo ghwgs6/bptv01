42767411724570f27bb1bd7dead6d001dc21a8f2
Gerhard Willemse <ghwgs6@gmail.com>
2026-09-03 14:09:42 +0200

    feat: 3.5-inch touchscreen ESP-NOW sensor simulator remote for 18-DI testing

--- yaml_editor/esp_now_sender.h
+++ yaml_editor/esp_now_sender.h
@@ -5,8 +5,10 @@
 #include <string.h>
 
 // Target Peers:
+// Broadcast MAC Address: FF:FF:FF:FF:FF:FF
 // Board 2 Receiver MAC Address: 8C:94:DF:6D:43:DC
 // 7" ESP32-P4 Display MAC Address: 80:F1:B2:D3:63:6D
+static uint8_t receiver_mac_broadcast[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 static uint8_t receiver_mac_board2[] = {0x8C, 0x94, 0xDF, 0x6D, 0x43, 0xDC};
 static uint8_t receiver_mac_display[] = {0x80, 0xF1, 0xB2, 0xD3, 0x63, 0x6D};
 
@@ -17,6 +19,13 @@ class ESPNowSenderComponent : public esphome::Component {
       ESP_LOGE("esp_now", "Error initializing ESP-NOW");
       return;
     }
+
+    // Add Broadcast Peer
+    esp_now_peer_info_t peerBroadcast = {};
+    memcpy(peerBroadcast.peer_addr, receiver_mac_broadcast, 6);
+    peerBroadcast.channel = 0;  
+    peerBroadcast.encrypt = false;
+    esp_now_add_peer(&peerBroadcast);
 
     // Add Board 2 Peer
     esp_now_peer_info_t peerBoard2 = {};
@@ -32,14 +41,15 @@ class ESPNowSenderComponent : public esphome::Component {
     peerDisplay.encrypt = false;
     esp_now_add_peer(&peerDisplay);
 
-    ESP_LOGI("esp_now", "ESP-NOW Sender Initialized! Peers: Board 2 & 7\" P4 Display added.");
+    ESP_LOGI("esp_now", "ESP-NOW Sender Initialized! Broadcast & Target Peers registered.");
   }
 
   void send_state(const char* state_str) {
-    // Broadcast / Send to both peers
+    // Broadcast & Unicast to peers
+    esp_now_send(receiver_mac_broadcast, (uint8_t *) state_str, strlen(state_str));
     esp_now_send(receiver_mac_board2, (uint8_t *) state_str, strlen(state_str));
     esp_now_send(receiver_mac_display, (uint8_t *) state_str, strlen(state_str));
-    ESP_LOGI("esp_now", "Sent ESP-NOW packet to Board 2 & 7\" Display: %s", state_str);
+    ESP_LOGI("esp_now", "Sent ESP-NOW packet: %s", state_str);
   }
 };
 
